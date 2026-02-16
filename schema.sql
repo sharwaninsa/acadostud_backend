b5094a5715776c3b72a0eaf83f09fa9651bdf0d1
Sharwan Kumar <sharwan981052@gmail.com>
2026-02-16 19:35:26 +0530

    Updated sql

--- schema.sql
+++ schema.sql
@@ -1,21 +1,30 @@
 create database acadostud_erp;
 
 create table faculty(
-    user_id serial primary key,
-    user_name varchar(255) not null default 'User Name',
-    user_email varchar(100) not null default 'user@example.com',
-    user_alternate_email varchar(100) not null default 'user@example.com',
-    user_phone varchar(10) not null default '8888888888',
-    user_alternate_phone varchar(10) not null default '9999999999',
-    user_gender varchar(20) not null default 'Male',
-    user_dob varchar(10) not null default "01-01-1970",
-    user_correspondence_address text not null default "Delhi",
-    user_permanent_address text not null default "Delhi",
-    user_aadhar_number varchar(12) not null default '999999999999',
-    user_pan_number varchar(10) not null default 'ABCDE1234F',
-    user_qualifications text not null default '[{"HSC": "Govt. Boys/Girls Senior Secondary School","Board": "CBSE", "Passign Year": 2008, "Max. Marks": 500, "Marks Obtained": 396}]',
-    user_photo_url text not null default 'src/img/avatar.png',
-    user_aadhar_image text not null default 'src/img/dummyUIDAI.png'
-    user_pan_image text not null default 'src/img/dummyPAN.png',
-    user_educational_documents text not null default 'src/pdf/dummyquali.pdf'
+    faculty_id serial primary key,
+    faculty_name varchar(255) not null default 'User Name',
+    faculty_email varchar(100) not null default 'user@example.com',
+    faculty_alternate_email varchar(100) not null default 'user@example.com',
+    faculty_phone varchar(10) not null default '8888888888',
+    faculty_alternate_phone varchar(10) not null default '9999999999',
+    faculty_gender varchar(20) not null default 'Male',
+    faculty_dob varchar(10) not null default "01-01-1970",
+    faculty_correspondence_address text not null default "Delhi",
+    faculty_permanent_address text not null default "Delhi",
+    faculty_aadhar_number varchar(12) not null default '999999999999',
+    faculty_pan_number varchar(10) not null default 'ABCDE1234F',
+    faculty_qualifications text not null default '[{"HSC": "Govt. Boys/Girls Senior Secondary School","Board": "CBSE", "Passign Year": 2008, "Max. Marks": 500, "Marks Obtained": 396}]',
+    faculty_qualifying_exams text not null default '[{"CTET": "2020"}]',
+    faculty_photo_url text not null default 'src/img/avatar.png',
+    faculty_aadhar_image text not null default 'src/img/dummyUIDAI.png'
+    faculty_pan_image text not null default 'src/img/dummyPAN.png',
+    faculty_educational_documents text not null default 'src/pdf/dummyquali.pdf',
+    faculty_creation_ip varchar(100) not null,
+    faculty_updation_ip varchar(100) not null,
+    faculty_creation_navigator text not null,
+    faculty_updation_navigator text not null,
+    faculty_creation_date timestamptz default current_timestamp,
+    faculty_updation_date timestamptz default current_timestampt,
+    faculty_creation_location text not null,
+    faculty_updation_location text not null
 )
